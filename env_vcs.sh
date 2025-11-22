feeecfb01f79ddf6a57ec58841c918e725db0259
lxf <lxf191375@163.com>
2025-11-22 16:13:59 +0800

    update chipyard

---
+++ env_vcs.sh
@@ -0,0 +1,17 @@
+export SNPSLMD_LICENSE_FILE=27000@amax
+export PATH="$PATH:/opt/riscv/bin"
+export VCS_HOME="/data0/tools/Synopsys/vcs/vcs/W-2024.09-SP1"
+export PATH="$PATH:$VCS_HOME/bin"
+export VERDI_HOME="/data0/tools/Synopsys/verdi/verdi/W-2024.09-SP1"
+export PATH="$PATH:$VERDI_HOME/bin"
+export SCL_HOME="/data0/tools/Synopsys/scl/scl/2024.06"
+export PATH="$PATH:$SCL_HOME/linux64/bin"
+export DC_HOME="/data0/tools/Synopsys/dc/syn/W-2024.09-SP1"
+export PATH="$PATH:$DC_HOME/bin"
+export PT_HOME="/data0/tools/Synopsys/ptpx/prime/W-2024.09-SP1/"
+export PATH="$PATH:$PT_HOME/bin"
+
+export LM_LICENSE_FILE=/data0/tools/Synopsys/lic/Synopsys.dat
+
+alias vcs="vcs -full64"
+alias lmli="lmgrd -c /data0/tools/Synopsys/lic/Synopsys.dat"
